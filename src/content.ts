20d124f7d4991f3f69d94232261284ba51b556ea
Tejas Angadi <tejasangadi456@gmail.com>
2026-01-14 16:20:14 +0530

    FEATURE: I have added the logic to extract the commit messages

--- src/content.ts
+++ src/content.ts
@@ -1,13 +1,44 @@
-const isGithubPRPage = () => {
+const isGithubPRPage = (): boolean => {
   const { hostname, pathname } = window.location;
 
-  console.log("HOSTNAME: ", hostname);
-  console.log("PATHNAME: ", pathname);
-
-  if (hostname === "github.com" && pathname.includes("/compare")) {
-    console.log("Hi from PR PAGE");
+  if (hostname !== "github.com" || !pathname.includes("/compare")) {
+    return false;
   }
+
+  // Testing log
+  console.log("✅ GitHub PR Page detected:", window.location.href);
+  return true;
 };
 
-console.log("✅ GitHub PR Page detected:", window.location.href);
-isGithubPRPage();
+const extractCommitMessages = (): HTMLCollectionOf<Element> => {
+  return document.getElementsByClassName(
+    "Link--primary text-bold js-navigation-open markdown-title",
+  );
+};
+
+const waitForCommits = (): Promise<HTMLCollectionOf<Element>> => {
+  return new Promise((resolve) => {
+    console.log({ resolve });
+    const interval = setInterval(() => {
+      const commits = extractCommitMessages();
+      if (commits.length > 0) {
+        clearInterval(interval);
+        resolve(commits);
+      }
+    }, 300);
+  });
+};
+
+const main = async () => {
+  // Checking whether the current page is github in PR Page or not
+  if (!isGithubPRPage()) return;
+
+  // Extracting commit messages using vanilla JS
+  const commitMessages = await waitForCommits();
+
+  Array.from(commitMessages).forEach((commitMessage) => {
+    console.log((commitMessage as HTMLElement).innerText);
+  });
+};
+
+main();
